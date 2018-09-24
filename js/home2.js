5690a79994b2d11f7a8a35454b1ada7d270f52a9
zaykha.kyawsan@bison.howard.edu <zaykha@zaykha-macbookpro.roam.corp.google.com>
2018-09-24 12:07:57 -0700

    Added draft logic to create a new Room and add it to the firebase database.

--- js/home2.js
+++ js/home2.js
@@ -18,3 +18,39 @@ function logout() {
 		// An error happened.
 	});
 }
+
+function validInput() {
+	var regex = /[^a-z0-9]/gi;
+	var roomName = document.getElementById('roomName').value;
+	if (regex.test(roomName)) {
+		alert('Room name cannot have special characters!');
+		document.getElementById('roomName').value = null;
+		return false;
+	}
+	// Prevent empty name field. Using attribute "required" requires a form, 
+	// which somehow does not update the firebase database.
+	if (roomName == "") {
+		alert('Room name cannot be empty!');
+		return false;
+	}
+	return true;
+}
+
+function createRoom() {
+	if (validInput()) {
+		const database = firebase.database();
+		var currUser = firebase.auth().currentUser;
+		database.ref('users/test').set({rooms: 'asdf'});
+		// window.location.href = "../html/room.html";
+	}
+}
+
+// Get the createRoomWindow
+var createRoomWindow = document.getElementById('createRoomWindow');
+
+// When the user clicks anywhere outside of the createRoomWindow, close it
+window.onclick = function(event) {
+    if (event.target == createRoomWindow) {
+        createRoomWindow.style.display = "none";
+    }
+}
