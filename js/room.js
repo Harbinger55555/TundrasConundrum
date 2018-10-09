91ae097837c2150d0b23c43409e98e83487b58be
zaykha.kyawsan@bison.howard.edu <zaykha@zaykha-macbookpro.roam.corp.google.com>
2018-10-09 13:01:54 -0700

    Answers now listed under one child of puzzles; Hints now listed under one child of puzzles.

--- js/room.js
+++ js/room.js
@@ -33,16 +33,33 @@ function createPuzzle() {
 		
 		// Get the currently logged in user.
 		var currUser = firebase.auth().currentUser.uid;
-		
+
 		// Checks if room belongs to the current User.
 		var currRoomInUser = firebase.database().ref().child('users/' + currUser + '/rooms/' + roomKey);
 		currRoomInUser.once('value', function(snapshot) {
 			if (snapshot.exists()){
-				currRoomInUser.child('puzzles').push().set({
-					question: document.getElementById('question').value,
-					correctAnswer: document.getElementById('correctAnswer').value,
-					wrongAnswer1: document.getElementById('wrongAnswer1').value
+				let newPuzzleKey = currRoomInUser.child('puzzles').push().key;
+				currRoomInUser.child('puzzles/' + newPuzzleKey).set({
+					question: document.getElementById('question').value
 				});
+				let puzzleAnswers = currRoomInUser.child('puzzles/' + newPuzzleKey + '/answers');
+				puzzleAnswers.set({
+					correct: document.getElementById('correctAnswer').value,
+					wrong1: document.getElementById('wrongAnswer1').value
+				});
+				// If optional fields are filled, update firebase RTDB.
+				let wrong2 = document.getElementById('wrongAnswer2').value;
+				if (wrong2 != "") puzzleAnswers.update({wrong2: wrong2});
+				let wrong3 = document.getElementById('wrongAnswer3').value
+				if (wrong3 != "") puzzleAnswers.update({wrong3: wrong3});
+				var puzzleHints = currRoomInUser.child('puzzles/' + newPuzzleKey + '/hints');
+				let hint1 = document.getElementById('hint1').value
+				if (hint1 != "") puzzleHints.update({hint1: hint1});
+				let hint2 = document.getElementById('hint2').value
+				if (hint2 != "") puzzleHints.update({hint2: hint2});
+				let hint3 = document.getElementById('hint3').value
+				if (hint3 != "") puzzleHints.update({hint3: hint3});
+
 				document.getElementById('createPuzzleWindow').style.display = "none";
 			}
 			else {
