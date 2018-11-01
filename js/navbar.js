741dddbe3018ec62f7d8de4a1f1df930eff9211c
Zaykha San <zaykha@zaykha-macbookpro.roam.corp.google.com>
2018-11-01 14:40:46 -0700

    Changed navbar to collapse on small screens.

--- js/navbar.js
+++ js/navbar.js
@@ -7,15 +7,29 @@ function logout() {
     });
 }
 
-window.onscroll = function() {stickyNavbar()};
+// window.onscroll = function() {stickyNavbar()};
+//
+// function stickyNavbar() {
+//     var navbar = document.getElementById("navbar");
+//     var navWrapper = document.getElementById("navWrapper");
+//     var sticky = navWrapper.offsetTop;
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
 
-function stickyNavbar() {
-    var navbar = document.getElementById("navbar");
-    var navWrapper = document.getElementById("navWrapper");
-    var sticky = navWrapper.offsetTop;
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+
+function myFunction() {
+    navbar_collapsed = !navbar_collapsed;
+    var topnav = document.getElementById("topnav");
+    if (!navbar_collapsed) {
+        topnav.classList.add("responsive");
     } else {
-        navbar.classList.remove("sticky");
+        topnav.classList.remove("responsive");
     }
 }
+
+// Default value is true since when the collapse icon appears, the navbar items will be collapsed.
+var navbar_collapsed = true;
