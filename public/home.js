a83edf7fdf205363addbb3268ec20f9fdc18a9e3
Zaykha San <zaykha@zaykha-macbookpro.roam.corp.google.com>
2018-11-12 13:50:20 -0800

    Implemented the looad popular rooms logic for home.html.

--- public/home.js
+++ public/home.js
@@ -6,6 +6,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 		// User still signed in.
 		if (user) {
 		    greetUser(user.uid);
+            loadPopularRooms();
 		}
 	}
 });
@@ -15,4 +16,42 @@ function greetUser(userId) {
     userName.once('value', function (snapshot) {
         document.getElementById("greeting").innerHTML = "Welcome " + (snapshot.val() || "Anonymous User");
     })
+}
+
+function loadPopularRooms() {
+    // Get the 3 most popular rooms from the RTDB.
+    // limitToLast(3) because orderByChild orders in ascending order.
+    var mostPopularRooms = firebase.database().ref('rooms').orderByChild('finishCount').limitToLast(3);
+    var mostPopularRoomsArr = [];
+
+    // ROOM_NUM is used to reference the respective roomImg DOM elements.
+    var ROOM_NUM = 0;
+    mostPopularRooms.once('value', function(snapshot) {
+        snapshot.forEach(function(room) {
+            mostPopularRoomsArr.push(room);
+        });
+    }).then(
+        () => {
+            // reverse() because roomImg DOM elements need to be filled in descending order.
+            mostPopularRoomsArr.reverse();
+            for (var room of mostPopularRoomsArr) {
+                ROOM_NUM += 1;
+                let roomID = room.key;
+                let roomName = room.val().name;
+                let roomImgUrl = room.val().themeURL;
+                // TODO: create a roomName div and fill in the roomName.
+                let roomImgElement = document.getElementById('room' + ROOM_NUM);
+                roomImgElement.setAttribute("src", (roomImgUrl || "./images/huh.png"));
+                roomImgElement.onclick = function() {
+                    playUnity(roomID);
+                };
+                roomImgElement.style.cursor = "pointer";
+            }
+        }
+    );
+}
+
+function playUnity(room) {
+    sessionStorage.setItem("playRoomKey", room);
+    window.location.href = "play.html";
 }
