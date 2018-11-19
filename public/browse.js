e2a2bcb8150c70159e05ef73f4dce85688257a76
Zaykha San <zaykha@zaykha-macbookpro.roam.corp.google.com>
2018-11-19 15:33:50 -0800

    Room sizes now shown in browse.

--- public/browse.js
+++ public/browse.js
@@ -13,7 +13,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 // Appends the room as a roomDiv into roomDivList.
-function appendRoom(roomImgUrl, roomDesc, roomName, roomID) {
+function appendRoom(roomImgUrl, roomDesc, roomName, roomID, puzzleCount) {
     // Creates a new div for the room (which will consists of a roomImgDiv, roomDescDiv, and roomNameDiv).
     let newRoomDiv = document.createElement('div');
     newRoomDiv.setAttribute("class", "roomDiv");
@@ -23,6 +23,24 @@ function appendRoom(roomImgUrl, roomDesc, roomName, roomID) {
     roomDescDiv.setAttribute("class", "roomDescDiv");
     let roomDescText = document.createTextNode(roomDesc || "No Description... Bask in the mystery!");
     roomDescDiv.appendChild(roomDescText);
+
+    // Wrap description and puzzleCount divs into their own div for formatting purposes.
+    let roomDescAndPuzzleCountDiv = document.createElement('div');
+    roomDescAndPuzzleCountDiv.appendChild(roomDescDiv);
+
+    // Identify room size according to its puzzleCount.
+    var puzzleCountText;
+    if (puzzleCount <= 5) {
+        puzzleCountText = document.createTextNode("Room Size: Small");
+    } else if (puzzleCount <= 15) {
+        puzzleCountText = document.createTextNode("Room Size: Moderate");
+    } else if (puzzleCount <= 30) {
+        puzzleCountText = document.createTextNode("Room Size: Large");
+    } else {
+        puzzleCountText = document.createTextNode("Room Size: Enormous");
+    }
+    roomDescAndPuzzleCountDiv.appendChild(puzzleCountText);
+    roomDescAndPuzzleCountDiv.setAttribute("class", "roomDescAndPuzzleCountDiv");
 
     // Creates a div for the room image.
     let roomImgDiv = document.createElement('div');
@@ -41,15 +59,15 @@ function appendRoom(roomImgUrl, roomDesc, roomName, roomID) {
     let roomNameText = document.createTextNode(roomName);
     roomNameDiv.appendChild(roomNameText);
 
-    //wrap name and image into their own div for formatting purposes
+    // Wrap name and image into their own div for formatting purposes.
     let roomNameAndImgDiv = document.createElement('div');
     roomNameAndImgDiv.appendChild(roomImgDiv);
     roomNameAndImgDiv.appendChild(roomNameDiv);
-    roomNameAndImgDiv.setAttribute( "class", "roomNameAndImg" );
+    roomNameAndImgDiv.setAttribute("class", "roomNameAndImg");
 
     // Append all three content divs into the newRoomDiv.
     newRoomDiv.appendChild(roomNameAndImgDiv);
-    newRoomDiv.appendChild(roomDescDiv);
+    newRoomDiv.appendChild(roomDescAndPuzzleCountDiv);
 
     let roomDivList = document.getElementById('roomDivList');
     roomDivList.appendChild(newRoomDiv);
@@ -76,7 +94,12 @@ function loadAllRooms() {
             let roomDesc = roomSnapshot.child('description').val();
             let roomName = roomSnapshot.child('name').val();
             let roomID = roomSnapshot.key;
-            appendRoom(roomImgUrl, roomDesc, roomName, roomID);
+            let puzzleCount = 0;
+            for (var _ in roomSnapshot.child('puzzles').val()) {
+                puzzleCount++;
+            }
+
+            appendRoom(roomImgUrl, roomDesc, roomName, roomID, puzzleCount);
         }
 
         // Hide the loader.
